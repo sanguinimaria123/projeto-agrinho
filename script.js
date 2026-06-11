da153b183a5ea7e735500fc8827f3fd7c575fafa
sanguinimaria123 <sanguini.maria@escola.pr.gov.br>
2026-06-11 11:28:30 +0000

    renomear imagens

---
+++ script.js
@@ -0,0 +1,82 @@
+let pontos = 50;
+let historico = [];
+
+// dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+// personalização
+let nome = prompt("Qual seu nome?");
+if (nome) {
+  document.getElementById("boasVindas").innerHTML =
+    nome + ", suas decisões moldam o futuro 🌎";
+}
+
+// simulador
+function simular() {
+  let pratica = document.getElementById("pratica").value;
+  let resultado = document.getElementById("resultado");
+  let barra = document.getElementById("nivel");
+
+  if (pratica === "convencional") {
+    pontos -= 10;
+    resultado.innerHTML = "🚜 Alta produção, alto impacto";
+  } 
+  else if (pratica === "organica") {
+    pontos += 10;
+    resultado.innerHTML = "🌿 Sustentabilidade máxima";
+  } 
+  else {
+    pontos += 5;
+    resultado.innerHTML = "🤖 Equilíbrio tecnológico";
+  }
+
+  pontos = Math.max(0, Math.min(100, pontos));
+
+  historico.push(pontos);
+  barra.style.width = pontos + "%";
+
+  desenharGrafico();
+  calcularRanking();
+}
+
+// gráfico
+function desenharGrafico() {
+  let canvas = document.getElementById("graficoCanvas");
+  let ctx = canvas.getContext("2d");
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  ctx.moveTo(0, 250 - historico[0]);
+
+  for (let i = 0; i < historico.length; i++) {
+    ctx.lineTo(i * 50, 250 - historico[i]);
+  }
+
+  ctx.stroke();
+}
+
+// ranking
+function calcularRanking() {
+  let ranking = document.getElementById("ranking");
+
+  if (pontos >= 80) {
+    ranking.innerHTML = "🏆 Produtor Sustentável de Elite";
+  } else if (pontos >= 50) {
+    ranking.innerHTML = "👍 Produtor Consciente";
+  } else {
+    ranking.innerHTML = "⚠️ Alto impacto ambiental";
+  }
+}
+
+// animação scroll
+window.addEventListener("scroll", () => {
+  document.querySelectorAll("section").forEach(sec => {
+    let topo = sec.getBoundingClientRect().top;
+    if (topo < window.innerHeight - 100) {
+      sec.classList.add("section-ativa");
+    }
+  });
+});
